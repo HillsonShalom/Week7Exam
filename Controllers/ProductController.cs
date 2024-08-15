31438fa3750c73aadf928cbac22c35f4fce45888
Hillson <hillsonshalom@gmail.com>
2024-08-15 17:18:14 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Week7Exam.Models;
+
+namespace Week7Exam.Controllers
+{
+    public class ProductController : Controller
+    {
+        private HttpClient _httpClient;
+        public ProductController()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task<IActionResult> Index()
+        {
+            string json = await _httpClient.GetStringAsync("https://fakestoreapi.com/products");
+            List<Product> list = JsonConvert.DeserializeObject<List<Product>>(json);
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            try
+            {
+                HttpContent content = new StringContent(JsonConvert.SerializeObject(product));
+                var res  = await _httpClient.PostAsync("https://fakestoreapi.com/products", content);
+                Console.WriteLine(res.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Week7Exam.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+    }
+}
